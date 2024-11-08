40c0049f18d35224d1f00b7a5c607e9b201e7faa
Priyanshu.sde <priyanshu.sde@gmail.com>
2024-11-08 22:57:20 +0530

    deleted Backend

--- server/model/todos.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const todoSchema = new mongoose.Schema({
-    title: String,
-    done: Boolean,
-    userId: mongoose.Schema.Types.ObjectId,
-  });
-
-  export default mongoose.model('Todo',todoSchema);
